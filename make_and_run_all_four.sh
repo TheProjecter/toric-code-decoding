0779d1b14b29a5147f2518bd8d459192595f3369
bcriger <bcriger@gmail.com>
2013-11-29 10:47:21 +0000

    Completed Profiling run on machine with 64 gb ram, checking in to get callgraphs on laptop

--- make_and_run_all_four.sh
+++ make_and_run_all_four.sh
@@ -3,16 +3,16 @@
 cd qecsim_noisy_concatenated
 make clean
 make
-./qecsim
+nice -n 19 ./qecsim
 cd ../qecsim_noisy_toric
 make clean
 make
-./qecsim
+nice -n 19 ./qecsim
 cd ../qecsim_perfect_concatenated
 make clean
 make
-./qecsim
+nice -n 19 ./qecsim
 cd ../qecsim_perfect_toric
 make clean
 make
-./qecsim
+nice -n 19 ./qecsim
